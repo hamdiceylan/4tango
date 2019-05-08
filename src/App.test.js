8fe296794a8cff46dd23e18b794a30e4f8d09231
Hamdi Ceylan <hamdiceylan@gmail.com>
2019-05-08 18:56:35 +0100

    initial commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import App from './App';
+import { shallow } from 'enzyme'
+import enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+enzyme.configure({ adapter: new Adapter() });
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+
+describe('renders without crashing', () => {
+  const app = shallow(<App />);
+
+  it('renders properly', () =>{
+    expect(app).toMatchSnapshot();
+  })
 });
